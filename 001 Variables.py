92636b5a1c85637294a630e588d460ceb8f28aa4
JulioReyesGonzalez <143145346+JulioReyesGonzalez@users.noreply.github.com>
2025-03-06 00:48:56 -0600

    Add files via upload

---
+++ 001 Variables.py
@@ -0,0 +1,16 @@
+
+# Julio Reyes
+
+#Variables con mensaje
+mensaje= "Bienvenidos a el curso de python"
+print(mensaje)
+
+#variables numericas
+num1 = 23
+num2 = 12
+
+resultado = num1+ num2
+mensaje = "Resultado de la suma:"
+print(mensaje)
+print(resultado)
+
