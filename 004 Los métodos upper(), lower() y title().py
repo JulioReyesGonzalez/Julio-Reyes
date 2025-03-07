03c40168b5355741d0a231724cfa7d5438b30349
JulioReyesGonzalez <143145346+JulioReyesGonzalez@users.noreply.github.com>
2025-03-07 10:30:07 -0600

    Create 004 Los métodos upper(), lower() y title().py
    Se implementaron métodos de formateo de strings en Python:
    
    title(): Convierte la primera letra de cada palabra en mayúscula.
    lower(): Convierte toda la frase a minúsculas.
    upper(): Convierte toda la frase a mayúsculas.
    Se usó print() para verificar los resultados en la consola.

---
+++ 004 Los métodos upper(), lower() y title().py
@@ -0,0 +1,13 @@
+# Poner en mayuscula la primera letra de cada palabra
+nombre = "julio reyes gonzalez"
+nombre_formateado = nombre,title()
+printf(nombre_formateado)
+
+#Como convertir una frase en minusculas
+frase= "Esta Es Una Frase Para Ser Formateada."
+frase_minusculas = frase.lower()
+print(frase_minusculas)
+
+#convertir una frase a matusculas
+frase_mayusculas=frase.upper()
+print(frase_mayusculas)
