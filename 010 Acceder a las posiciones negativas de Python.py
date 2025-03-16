de7cfd1291d0f4d6fce2fdc2547bdc23caf962ee
JulioReyesGonzalez <143145346+JulioReyesGonzalez@users.noreply.github.com>
2025-03-15 23:02:45 -0600

    Acceso a elementos de una lista índices negativo
    Se implementa el acceso a elementos de una lista utilizando índices negativos. Se acceden e imprimen los colores 'naranja', 'amarillo', 'lila', 'blanco' y 'rojo' desde una lista predefinida.

---
+++ 010 Acceder a las posiciones negativas de Python.py
@@ -0,0 +1,17 @@
+# Definir la lista de colores
+colores = ['rojo', 'azul', 'verde', 'amarillo', 'marron', 'lila', 'negro', 'rosa', 'blanco', 'naranja']
+
+# Acceder a los colores utilizando indices negativos
+color_naranja = colores[-1]   # Último elemento (-1)
+color_amarillo = colores[-7]  # Séptimo desde el final (-7)
+color_lila = colores[-5]      # Quinto desde el final (-5)
+color_blanco = colores[-2]    # Segundo desde el final (-2)
+color_rojo = colores[-10]     # Décimo desde el final (-10), que es el primero
+
+# Imprimir los resultados
+print(f"Naranja: {color_naranja}")   # Salida esperada: naranja
+print(f"Amarillo: {color_amarillo}") # Salida esperada: amarillo
+print(f"Lila: {color_lila}")         # Salida esperada: lila
+print(f"Blanco: {color_blanco}")     # Salida esperada: blanco
+print(f"Rojo: {color_rojo}")         # Salida esperada: rojo
+
