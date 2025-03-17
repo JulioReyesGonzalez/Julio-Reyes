32ab0234c71bf0064baa27378b3fc5657840b5c0
JulioReyesGonzalez <143145346+JulioReyesGonzalez@users.noreply.github.com>
2025-03-16 23:17:28 -0600

    eliminar elementos de una lista con remove()
    escripción del commit: En este commit, se elimina de la lista colores los elementos 'rojo' y 'amarillo' utilizando el método remove(). Este método busca y elimina la primera aparición del valor especificado.

---
+++ 012 eliminar elementos de listas Python con remove().py
@@ -0,0 +1,11 @@
+# Definimos una lista llamada 'colores' que contiene varios colores
+colores = ['rojo', 'azul', 'verde', 'amarillo', 'marron', 'lila', 'negro']
+
+# Usamos el método 'remove()' para eliminar el elemento 'rojo' de la lista
+colores.remove('rojo')   # Elimina 'rojo'
+
+# Usamos el método 'remove()' para eliminar el elemento 'amarillo' de la lista
+colores.remove('amarillo')  # Elimina 'amarillo'
+
+# Imprimimos la lista después de eliminar los elementos
+print(colores)
