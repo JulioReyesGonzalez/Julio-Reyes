d1bc130a4e40358c83a567db5e1cf8ad0a03d948
JulioReyesGonzalez <143145346+JulioReyesGonzalez@users.noreply.github.com>
2025-03-17 10:10:22 -0600

    El condicional IF
    Modificado el operador de comparación en el primer ejercicio para que la condición sea True (!= en lugar de ==).
    Cambiado el operador en el segundo ejercicio para que la condición sea True (> en lugar de <).
    Ajustado el valor de num1 en el tercer ejercicio para que la condición sea False sin modificar el operador.

---
+++ 019 El condicional IF de Python.py
@@ -0,0 +1,24 @@
+# Ejercicio 1: Cambiar el operador para que la condicion sea True
+num1 = 15
+num2 = 20
+
+# Usamos el operador de desigualdad "!=" en lugar de "==" para que la condicion sea True
+if num1 != num2:
+    print('Ejercicio 1: Se ejecuta el if.')
+
+# Ejercicio 2: Cambiar el operador para que la condicion sea True
+num1 = 1450
+num2 = 60
+
+# Usamos ">" en lugar de "<" para que la condicion sea True
+if num1 > num2:
+    print('Ejercicio 2: Se ejecuta el if.')
+
+# Ejercicio 3: Hacer que el condicional sea False sin cambiar el operador
+num1 = 60  # Cambiamos el valor de num1 para que sea igual a num2
+num2 = 60
+
+# La condición num1 != num2 ahora es False, por lo que no se ejecutará el if
+if num1 != num2:
+    print('Ejercicio 3: Se ejecuta el if.')  # Esta linea no se imprimirá
+
