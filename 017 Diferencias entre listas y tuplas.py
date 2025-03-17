cba86db00617ae546fe65b72e8314f5061dbde74
JulioReyesGonzalez <143145346+JulioReyesGonzalez@users.noreply.github.com>
2025-03-16 23:55:21 -0600

    Solucionar ejercicios con tuplas y operaciones matemáticas
    En este commit, se resuelven dos ejercicios:
    
    Se imprime el segundo elemento de la tupla colores (índice 1).
    Se realiza una operación matemática utilizando los elementos de la tupla numeros para obtener el resultado 25 mediante suma y resta.

---
+++ 017 Diferencias entre listas y tuplas.py
@@ -0,0 +1,16 @@
+# Ejercicio 1: Imprimir la segunda posicion de la tupla
+colores = ('rojo', 'azul', 'verde', 'amarillo', 'marron', 'lila', 'negro', 'rosa', 'blanco', 'naranja')
+
+# Imprimir la segunda posicion de la tupla, recordando que los índices empiezan en 0
+print(colores[1])  # 'azul' es el color en la posición 1
+
+
+# Ejercicio 2: Obtener el resultado 25 utilizando suma y resta
+numeros = (10, 1, 5, 11)
+
+# Realizar la operacion para obtener 25
+operacion = numeros[0] + numeros[2] + numeros[3] - numeros[1]
+
+# Imprimir el resultado
+print(operacion)  # Resultado esperado: 25
+
