8a63b787c1ad4ba29cc819596829040636f1e335
JulioReyesGonzalez <143145346+JulioReyesGonzalez@users.noreply.github.com>
2025-03-16 23:45:14 -0600

    el método sort() para ordenar listas alfabeticamen
    En este commit, se ordena la lista colores en orden alfabético descendente utilizando el método sort() con el parámetro reverse=True. Esto garantiza que los colores aparezcan desde la letra 'z' hasta la 'a'.

---
+++ 016 El método sort().py
@@ -0,0 +1,9 @@
+# Lista original de colores
+colores = ['rojo', 'azul', 'verde', 'amarillo', 'marron', 'lila', 'negro', 'rosa', 'blanco', 'naranja']
+
+# Usamos el metodo 'sort()' para ordenar la lista en orden alfabetico descendente
+colores.sort(reverse=True)
+
+# Imprimimos la lista ordenada
+print(colores)
+
