0063a9fe1bc49b7f12cba55d532fc0c95fade7be
JulioReyesGonzalez <143145346+JulioReyesGonzalez@users.noreply.github.com>
2025-03-16 23:31:43 -0600

    Insertar elementos en listas Python con append()
    En este commit, se añaden los colores 'fuxia' y 'celeste' al final de la lista colores utilizando el método append(). Este método se usa para agregar elementos al final de la lista. Los comentarios explicativos están incluidos para ayudar a entender el proceso.

---
+++ 014 Insertar elementos en listas Python con append().py
@@ -0,0 +1,12 @@
+# Lista original de colores
+colores = ['rojo', 'azul', 'verde', 'amarillo', 'marron', 'lila', 'negro', 'rosa', 'blanco', 'naranja']
+
+# Usamos el metodo 'append()' para añadir el color 'fuxia' al final de la lista
+colores.append('fuxia')
+
+# Usamos el metodo 'append()' para añadir el color 'celeste' al final de la lista
+colores.append('celeste')
+
+# Imprimimos la lista después de añadir los nuevos colores
+print(colores)
+
